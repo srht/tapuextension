e88d5e795ac0a21b2d20346829b03e4d67575cc5
Serhat Köroğlu <srhtkrgl@outlook.com>
2023-09-22 16:45:56 +0300

    first commit

---
+++ content.js
@@ -0,0 +1,19 @@
+// content.js
+
+document.addEventListener("click", function (event) {
+  // Check if the clicked element matches your criteria (e.g., element with a specific class or ID)
+  if (event.target.matches(".tiklanacak")) {
+    // Send an XHR request here
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:5240/", true);
+    xhr.onreadystatechange = function () {
+      console.log(xhr.responseText);
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        document.querySelector("body").innerHTML += xhr.responseText;
+        let responseJSON = JSON.parse(xhr.responseText);
+        window.console.log(responseJSON.geometry.coordinates);
+      }
+    };
+    xhr.send();
+  }
+});
