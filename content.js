b315268b0f4aeebd7ea065fd15e7d347a328b75f
Serhat Köroğlu <srhtkrgl@outlook.com>
2023-09-22 16:58:55 +0300

    maps button selector

--- content.js
+++ content.js
@@ -2,6 +2,9 @@
 
 document.addEventListener("click", function (event) {
   // Check if the clicked element matches your criteria (e.g., element with a specific class or ID)
+  //document.querySelectorAll('div[role="dialog"] button')
+  let btns = event.target.querySelector('div[role="dialog"] button');
+  window.console.log(btns);
   if (event.target.matches(".tiklanacak")) {
     // Send an XHR request here
     var xhr = new XMLHttpRequest();
